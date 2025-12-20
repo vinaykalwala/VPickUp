e4152e84edb18ea532a0205afec5a8ef2e577faf
LaxmiPrasanna-2593 <laxmiprasanna2593@gmail.com>
2025-12-20 10:45:49 +0530

    basic frontend structure created

--- Pickup/views.py
+++ Pickup/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def terms(request):
+    return render(request, "terms.html")
+
+def privacy(request):
+    return render(request, "privacy.html")
+
+def faq(request):
+    return render(request, "faq.html")
