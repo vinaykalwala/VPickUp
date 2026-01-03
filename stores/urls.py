afefd3d0b941ab29938748a5153262ea1fcd0a68
Durga Prakash Shilarapu <109608138+Durgaprakash1102@users.noreply.github.com>
2026-01-03 17:38:12 +0530

    Customer Side stores

--- stores/urls.py
+++ stores/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
     path('stores/<int:store_id>/verification/', StoreVerificationView.as_view(), name='store_verification'),
     path('stores/verificationlist/',AdminStoreVerificationListView.as_view(),name='admin_store_verification_list'),
     path('stores/<int:store_id>/verify/', AdminStoreVerifyView.as_view(), name='admin_store_verify'),
+    path('customer/store/<int:store_id>/', CustomerStoreDetailView.as_view(), name='customer_store_detail'),
+    path('customer/store/<int:store_id>/category/<slug:category_slug>/',StoreCategoryView.as_view(),name='store_category'),
+    path('customer/store/<int:store_id>/subcategory/<slug:subcategory_slug>/',StoreSubCategoryProductsView.as_view(), name='store_subcategory_products'),
+
 ]
